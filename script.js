ecf1142f0ebdf726e7701377f3f965ff2cf42b03
zhi-gen <ruusgendri3@gmail.com>
2025-08-16 14:59:00 +0800

    feat: redesign portfolio to match new style

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-// Mengaktifkan mode ketat untuk penulisan kode yang lebih baik
 'use strict';
 
-// Inisialisasi AOS (Animate On Scroll)
-AOS.init({
-    duration: 1000, // Durasi animasi dalam milidetik
-    once: true,     // Animasi hanya berjalan sekali saat scroll
-    offset: 50,     // Memicu animasi sedikit lebih awal
-});
-
-// Opsi untuk animasi mengetik (Typed.js)
 const typedOptions = {
-    strings: ['Junior Developer', 'Tech Enthusiast', 'Problem Solver'],
-    typeSpeed: 50,  // Kecepatan mengetik
-    backSpeed: 30,  // Kecepatan menghapus
-    backDelay: 2000, // Jeda sebelum mulai menghapus
-    startDelay: 500, // Jeda sebelum mulai mengetik
-    loop: true,     // Mengulang animasi
+    strings: [' make awesome websites.', ' build digital solutions.', ' design modern interfaces.'],
+    typeSpeed: 40,
+    backSpeed: 20,
+    backDelay: 2000,
+    startDelay: 500,
+    loop: true,
     showCursor: true,
     cursorChar: '|',
 };
 
-// Inisialisasi Typed.js pada elemen dengan id 'typed-text'
 const typed = new Typed('#typed-text', typedOptions);
